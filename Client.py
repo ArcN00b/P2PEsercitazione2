720aceeff74f8c3b521b138d312195a8b33bf3f3
Flavio <cavolate09@gmail.com>
2016-03-23 19:11:27 +0100

    Il main handler ora gestisce due connessioni sia Ipv4 che ipv6, il client finto genera e invia pacchetti a entrambe in maniera casuale

--- Client.py
+++ Client.py
@@ -1,11 +1,22 @@
 import socket
 import sys
 import time
+import random
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while True:
 
-sock.connect(("localhost", 3000))
-sock.sendall(("Ciao "+sys.argv[1]).encode())
+    r=random.randrange(0,100)
+    if r<50:
+        a='192.168.0.5'
+        b='IPV4 '
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+        a='::1'
+        b='IPV6 '
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
-time.sleep(30)
-sock.close()
+    sock.connect((a, 2545))
+    sock.sendall((b+sys.argv[1]).encode())
+
+    time.sleep(2)
+    sock.close()
--- Main.py
+++ Main.py
@@ -6,15 +6,20 @@ import threading
 
 class Peer:
 
-    def __init__(self,ip):
-        self.ip=ip
-        self.port=3000
+    def __init__(self,ipv4,ipv6):
+        self.ipv4=ipv4
+        self.ipv6=ipv6
+        self.port=2545
         self.stop_queue = queue.Queue(1)
-        u = ReceiveServer(self.stop_queue,self.ip,self.port,(3,self.ip,self.port))
-        self.server_thread = threading.Thread(target=u)#crea un thread e gli assa l'handler per il server da far partire
+        u1 = ReceiveServerIPV4(self.stop_queue,self.ipv4,self.port,(3,self.ipv4,self.port))
+        self.server_thread = threading.Thread(target=u1)#crea un thread e gli assa l'handler per il server da far partire
+        u2 = ReceiveServerIPV6(self.stop_queue,self.ipv6,self.port,(3,self.ipv6,self.port))
+        self.server_threadIP6 = threading.Thread(target=u2)
         self.server_thread.start()#parte
+        self.server_threadIP6.start()
 
-class ReceiveServer(asyncore.dispatcher):
+
+class ReceiveServerIPV4(asyncore.dispatcher):
     """Questa classe rappresenta un server per accettare i pacchetti
     degli altri peer."""
     def __init__(self, squeue, ip, port, data_t):
@@ -33,6 +38,25 @@ class ReceiveServer(asyncore.dispatcher):
         while self.squeue.qsize() == 0:
             asyncore.loop(timeout=1, count=5)
 
+class ReceiveServerIPV6(asyncore.dispatcher):
+    """Questa classe rappresenta un server per accettare i pacchetti
+    degli altri peer."""
+    def __init__(self, squeue, ip, port, data_t):
+        asyncore.dispatcher.__init__(self)
+        self.squeue = squeue
+        self.data_t = data_t #max near, mio ip e mia porta
+        self.create_socket(socket.AF_INET6,socket.SOCK_STREAM)#crea socket ipv6
+        self.set_reuse_addr()#riusa indirizzo, evita problemi indirizzo occupato
+        self.bind((ip, port)) #crea la bind del mio ip e porta
+        self.listen(5)# sta in ascolto di 5 persone max
+
+    def handle_accepted(self, socket_peer, address_peer):
+        handler = ReceiveHandler(socket_peer, address_peer, self.data_t)
+
+    def __call__(self):
+        while self.squeue.qsize() == 0:
+            asyncore.loop(timeout=1, count=5)
+
 class ReceiveHandler(asyncore.dispatcher_with_send):
 
     def __init__(self, conn_sock, near_address, data):
@@ -40,11 +64,12 @@ class ReceiveHandler(asyncore.dispatcher_with_send):
         self.near_address = near_address
         self.data_tuple = data
 
+    # Questo e il metodo che viene chiamato quando ci sono delle recive
     def handle_read(self):
         msg=self.recv(2048)
         print(msg.decode())
 
-p=Peer('localhost')
+p=Peer('192.168.05','::1')
 while True:
     sel=input("Inserisci qualcosa ")
     print(sel)
